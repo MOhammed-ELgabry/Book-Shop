ddf0b6c330703484bb302529132c375fd1ad7b67
MOhammed-ELgabry <mohammedelgabry187@gmail.com>
2026-01-27 23:22:31 +0200

    for-ver

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+   const nanvigate=useNavigate()
   return (
-    <div className='h-dvh w-[100%] bg-red-500'>
-      home page
+    <div className='h-dvh w-[100%] flex justify-center gap-4 bg-red-300'>
+      <button onClick={()=>{nanvigate('/register')}} className='btn btn-success text-white text-3xl font-bold'>register</button>
+      <button onClick={()=>{nanvigate('/login')}} className='btn btn-bg-black text-white text-3xl font-bold'>login</button>
+      <button></button>
     </div>
   )
 }
